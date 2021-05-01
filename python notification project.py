b4382ba91d51af7c0f647425f95f757e7a888d39
coding-bastard <aryanyadav0521@gmail.com>
2021-05-01 16:37:42 +0530

    Initial commit

---
+++ python notification project.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__=="__main__":
+    while True:
+        notification.notify(
+            title = "Please Drink Water",
+            message ="Water makes up a large part of your joint cartilage that helps absorb shock and make bone-against-bone movements smoother. Water also can help keep gout (a painful joint condition) at bay. It helps flush toxins from your body that could inflame your joints",
+            app_icon="C:\\Users\\WIndows\\Desktop\\New folder\\gallery\\icon.ico",
+            timeout=2
+        )
+        time.sleep(6)
